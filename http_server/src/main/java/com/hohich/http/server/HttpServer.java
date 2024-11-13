ff4505ff04facd7f116325e538afad97b64b569f
Hohichh <verkovich2005@gmail.com>
2024-11-14 01:34:37 +0300

    Get method update

--- http_server/src/main/java/com/hohich/http/server/HttpServer.java
+++ http_server/src/main/java/com/hohich/http/server/HttpServer.java
@@ -5,45 +5,45 @@ import com.hohich.http.messages.*;
 import java.net.*;
 import java.io.*;
 
-public class HttpServer{
+public class HttpServer {
     private final static String CONTENT_PATH = "D:\\Study\\Univer\\3_kurs\\AIPOS\\lab5\\http1-1_server\\hosted_files";
 
     public static void main(String[] args) {
-        try(ServerSocket server = new ServerSocket(
+        try (ServerSocket server = new ServerSocket(
                 8080, 50, InetAddress.getByName("localhost"))) {
             //listening
-            while(true){
-                try{
+            while (true) {
+                try {
                     Socket client = server.accept();
                     //handling client
                     new Thread(() -> handleClient(client)).start();
                     System.out.println("Client connected: " + client.getInetAddress().getHostAddress());
-                } catch(IOException e){
+                } catch (IOException e) {
                     System.out.println("Error accepting connection: " + e.getMessage());
                 }
             }
 
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Could not start server: " + e.getMessage());
-        }finally{
+        } finally {
             System.out.println("Server stopped.");
         }
     }
 
-    private static void handleClient(Socket cs){
+    private static void handleClient(Socket cs) {
         try (cs;
-            InputStream in = cs.getInputStream();
-            OutputStream out = cs.getOutputStream()) {
+             InputStream in = cs.getInputStream();
+             OutputStream out = cs.getOutputStream()) {
             // client request handling
             System.out.println("Handling client: " + cs.getInetAddress());
 
-            byte[] buf = new byte[1024*64];
+            byte[] buf = new byte[1024 * 64];
             int bytesRead = in.read(buf);
 
             String request = new String(buf, 0, bytesRead);
             HttpRequest req = new HttpRequest(request);
 
-            switch(req.getMethod()){
+            switch (req.getMethod()) {
                 case "GET":
                     handleGetRequest(req, out);
                     break;
@@ -75,67 +75,74 @@ public class HttpServer{
 
     private static void handleGetRequest(HttpRequest req,
                                          OutputStream out) throws IOException {
-            String uriPath = parseUriToPath(req.getUri());
-            File file = new File(CONTENT_PATH + uriPath);
-            //файл не найден
-            if(!file.exists()){
-                HttpResponse content_access_err = new HttpResponse(404, "Not Found",
-                        "<html>\n" +
-                        "<head><title>404 Not Found</title></head>\n" +
-                        "<body>\n" +
-                        "<h1>404 Not Found</h1>\n" +
-                        "<p>The requested file was not found on this server.</p>\n" +
-                        "</body>\n" +
-                        "</html>");
-                content_access_err.addHeader("Content-Type", "text/html");
-                content_access_err.addHeader("Content-Length", String.valueOf(
-                        content_access_err.getBody().length()));
+        String uriPath = parseUriToPath(req.getUri());
+        File file = new File(CONTENT_PATH + uriPath);
 
-                out.write(content_access_err.getResponse().getBytes());
-                out.flush();
-                return;
-            }
+        if (!file.exists()) {
+            HttpResponse content_access_err = new HttpResponse(404, "Not Found",
+                    "<html>\n" +
+                            "<head><title>404 Not Found</title></head>\n" +
+                            "<body>\n" +
+                            "<h1>404 Not Found</h1>\n" +
+                            "<p>The requested file was not found on this server.</p>\n" +
+                            "</body>\n" +
+                            "</html>");
+            content_access_err.addHeader("Content-Type", "text/html");
+            content_access_err.addHeader("Content-Length", String.valueOf(
+                    content_access_err.getBody().length()));
 
-            StringBuilder contentBuilder = new StringBuilder();
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    contentBuilder.append(line).append("\n");
-                }
-            } catch(IOException e){
-                System.out.println("Error reading file: " + e.getMessage());
-                HttpResponse internalErrResp = new HttpResponse(
-                        500, "Internal Server Error", "<html>\n" +
-                        "<head><title>500 Internal Server Error</title></head>\n" +
-                        "<body>\n" +
-                        "<h1>500 Internal Server Error</h1>\n" +
-                        "<p>An error occurred while reading the requested file.</p>\n" +
-                        "</body>\n" +
-                        "</html>");
-                internalErrResp.addHeader("Content-Type", "text/html");
-                internalErrResp.addHeader("Content-Length", String.valueOf(
-                        internalErrResp.getBody().length()));
-                out.write(internalErrResp.getResponse().getBytes());
-                out.flush();
-                return;
-            }
-
-            String respBody = contentBuilder.toString();
-            HttpResponse response = new HttpResponse(200, "OK", respBody);
-            response.addHeader("Content-Type", "text/html");
-            response.addHeader("Content-Length", String.valueOf(respBody.length()));
-
-            out.write(response.getResponse().getBytes());
+            out.write(content_access_err.getResponse().getBytes());
             out.flush();
+            return;
+        }
+        //define mime-type
+        String contentType = defineContentType(uriPath);
+
+        HttpResponse response = new HttpResponse(200, "OK", "");
+        response.addHeader("Content-Type", contentType);
+        response.addHeader("Content-Length", String.valueOf(file.length()));
+
+        out.write(response.getResponse().getBytes());
+        out.flush();
+
+        //send request body
+        try(FileInputStream fis = new FileInputStream(file)) {
+            byte[] buf = new byte[8192];
+            int bytesRead;
+            while(-1 != (bytesRead = fis.read(buf))) {
+                out.write(buf, 0, bytesRead);
+            }
+            out.flush();
+        } catch(IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            HttpResponse inErrResponse = new HttpResponse(500, "Internal Server Error",
+                    "<html>\n" +
+                    "<head><title>500 Internal Server Error</title></head>\n" +
+                    "<body>\n" +
+                    "<h1>500 Internal Server Error</h1>\n" +
+                    "<p>Unable to get content</p>\n" +
+                    "</body>\n" +
+                    "</html>");
+
+            out.write(inErrResponse.getResponse().getBytes());
+            out.flush();
+        }
     }
 
-    private static void handlePostRequest(HttpRequest req, Socket cs,
-                                          InputStream in, OutputStream out) throws IOException {
+    private static void handlePostRequest(HttpRequest req, OutputStream out) throws IOException {
+        String postedContent = req.getBody();
+        System.out.println("Received posted content: " + postedContent);
 
+        String respBody = "{\"message\": \"Data received\"}";
+        HttpResponse resp = new HttpResponse(200, "OK", respBody);
+        resp.addHeader("Content-Type", "text/html");
+        resp.addHeader("Content-Length", String.valueOf(respBody.length()));
+        out.write(resp.getResponse().getBytes());
+        out.flush();
     }
 
     private static void handleOptionsRequest(
-                                             OutputStream out) throws IOException {
+            OutputStream out) throws IOException {
         HttpResponse optionsResponse = new HttpResponse(200, "OK", "");
         optionsResponse.addHeader("Allow", "GET, POST, OPTIONS");
         optionsResponse.addHeader("Content-Length", "0");
@@ -151,6 +158,15 @@ public class HttpServer{
         }
         String path = uri.substring(pathStartIndex);
         return path.replace("/", "\\");
+    }
+
+    private static String defineContentType(String uriPath){
+        if (uriPath.endsWith(".html")) return "text/html";
+        if (uriPath.endsWith(".css")) return "text/css";
+        if (uriPath.endsWith(".js")) return "application/javascript";
+        if (uriPath.endsWith(".png")) return "image/png";
+        if (uriPath.endsWith(".svg")) return "image/svg+xml";
+        return "application/octet-stream";
     }
 
 }
